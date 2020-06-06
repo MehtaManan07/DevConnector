bea7414f959c7ababec86e1cc2926784e570b57e
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-06 19:14:44 +0530

    Added route for getting all profile and profile by user id

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useFindAndModify: false,
     });
     console.log("mongodb connected");
   } catch (error) {
