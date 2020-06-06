64a08f6a81ed235928ed0b370b032e822dc865c9
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-06 20:21:43 +0530

    Added PUT route for adding experience to the profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,19 +139,71 @@ router.get("/user/:user_id", async (req, res) => {
 //@desc    Delete profile, posts and posts
 //@access  Private
 
-router.delete("/", auth,async (req, res) => {
-  try { 
-      //@todo - remove user's post
+router.delete("/", auth, async (req, res) => {
+  try {
+    //@todo - remove user's post
 
-      //Remove profile
-    await Profile.findOneAndRemove({ user: req.user.id })
+    //Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
     // Remove user
-    await Profile.findOneAndRemove({ _id: req.user.id })
-    res.json({ msg: 'User deleted' });
+    await Profile.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: "User deleted" });
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server error");
   }
 });
 
+//@route   PUT api/profile/experience
+//@desc    Add profile experience
+//@access  Private
+
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is required").not().isEmpty(),
+      check("company", "Company").not().isEmpty(),
+      check("from", "From date is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+    try {
+        const profile = await Profile.findOne({user: req.user.id})
+        console.log(profile)
+
+        profile.experience.unshift(newExp);
+
+        await profile.save()
+        res.json(profile)
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 module.exports = router;
