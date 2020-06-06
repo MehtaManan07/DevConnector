0a256c12aaa702c5d8315162c45984124b13232b
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-06 14:59:41 +0530

    Added routes

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.get('/',(req,res) => {
     res.send('API Running')
 })
 
+//Define routes
+
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/profile', require('./routes/api/profile'))
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,() => {`Server started on port ${PORT}`})
