11a38c97383f814fbaa2871701a328f0d4772404
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-07 15:20:41 +0530

    Added login page and minor changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -11,13 +11,13 @@ const Navbar = () => {
       </h1>
       <ul>
         <li>
-          <Link to="profiles">Developers</Link>
+          <Link to="/profiles">Developers</Link>
         </li>
         <li>
-          <Link to="register">Register</Link>
+          <Link to="/register">Register</Link>
         </li>
         <li>
-          <Link to="login">Login</Link>
+          <Link to="/login">Login</Link>
         </li>
       </ul>
     </nav>
