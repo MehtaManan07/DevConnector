4072189a3189f00aa4d1d7aa81113888058e7199
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-07 14:34:11 +0530

    Added routes for register and login page

--- client/src/components/auth/register.js
+++ client/src/components/auth/register.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const register = () => {
+const Register = () => {
     return (
         <div>
             Register page
@@ -8,4 +8,4 @@ const register = () => {
     )
 }
 
-export default register
+export default Register
