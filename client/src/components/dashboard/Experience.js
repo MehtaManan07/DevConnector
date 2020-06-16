bc1d027b39fbf664e7324ddcf6f4298c60d0dd40
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-16 22:52:26 +0530

    Added postItem semi-static component

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
 import { connect } from "react-redux";
-import { deleteExperience } from '../../actions/profile'
+import { deleteExperience } from "../../actions/profile";
 
 const Experience = ({ experience, deleteExperience }) => {
   const experiences = experience.map((exp) => (
@@ -18,7 +18,13 @@ const Experience = ({ experience, deleteExperience }) => {
         )}
       </td>
       <td>
-        <button onClick={() => deleteExperience(exp._id)} className="btn btn-danger"> Delete </button>
+        <button
+          onClick={() => deleteExperience(exp._id)}
+          className="btn btn-danger"
+        >
+          {" "}
+          Delete{" "}
+        </button>
       </td>
     </tr>
   ));
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,26 +1,40 @@
-import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { getPosts } from '../../actions/post'
-import { useEffect } from 'react'
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getPosts } from "../../actions/post";
+import { useEffect } from "react";
+import PostItem from './PostItem'
+import Spinner from "../layout/Spinner";
+
 const Posts = ({ getPosts, post: { posts, loading } }) => {
-    useEffect(() => {
-        getPosts()
-    }, [getPosts])
-    return (
-        <Fragment>
-            
-        </Fragment>
-    )
-}
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  return !loading ? (
+    <Fragment>
+      <h1 className="text-primary large"> Posts </h1>
+      <p className="lead">
+          <i className="fas fa-user"></i> Welcome to the community
+      </p>
+      {/* Post form  */}
+      <div className="posts">
+          {posts.map(post => (
+              <PostItem key={post._id} post={post} />
+          ))}
+      </div>
+    </Fragment>
+  ) : (
+    <Spinner />
+  );
+};
 
 Posts.propTypes = {
-    getPosts: PropTypes.func.isRequired,
-    post: PropTypes.object.isRequired,
-}
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
 
-const mapStateToProps = state => ({
-    post: state.post
-})
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
 
-export default connect(mapStateToProps, { getPosts })(Posts)
+export default connect(mapStateToProps, { getPosts })(Posts);
