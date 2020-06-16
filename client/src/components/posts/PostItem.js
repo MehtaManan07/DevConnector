474aae1e999f679aebd619688e4d078256303d5a
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-06-17 00:36:50 +0530

    Deleting a post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { connect } from "react-redux";
-import { addLike, removeLike } from '../../actions/post'
+import { addLike, removeLike, deletePost } from "../../actions/post";
 const PostItem = ({
-  addLike, removeLike, 
+  addLike,
+  removeLike,
+  deletePost,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
 }) => {
@@ -23,33 +25,52 @@ const PostItem = ({
           <p className="post-date">
             Posted on <Moment format="YYYY/MM/DD"> {date} </Moment>
           </p>
-          <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
-            <i className="fas fa-thumbs-up"></i> {' '}
-            <span>{ likes.length > 0 && likes.length}</span>
+          <button
+            onClick={(e) => addLike(_id)}
+            type="button"
+            className="btn btn-light"
+          >
+            <i color="blue" className="fas fa-thumbs-up"></i>{" "}
+            <span>{likes.length > 0 && likes.length}</span>
           </button>
-          <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
+          <button
+            onClick={(e) => removeLike(_id)}
+            type="button"
+            className="btn btn-light"
+          >
             <i className="fas fa-thumbs-down"></i>
           </button>
           <Link to={`/post/${_id}`} className="btn btn-primary">
-            Discussion {' '} <span>{ comments.length > 0 && comments.length}</span>
+            Discussion <span>{comments.length > 0 && comments.length}</span>
           </Link>
           {!auth.loading && user === auth.user._id && (
-            <button type="button" className="btn btn-danger">
-              <i className="fas fa-times"></i>
+            <button
+              onClick={() => deletePost(_id)}
+              type='button'
+              className='btn btn-danger'
+            >
+              <i className='fas fa-times' />
             </button>
           )}
         </div>
-      </div>
     </div>
-  );
-};
+  </div>
+  
+)}
+
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
+  post: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
+  deletePost: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+  PostItem
+);
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,4 @@
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "../actions/types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "../actions/types";
 
 const initialState = {
   posts: [],
@@ -23,6 +23,12 @@ export default (state = initialState, { type, payload }) => {
         ),
         loading: false,
       };
+    case DELETE_POST:
+      return {
+        ...state, 
+        posts: state.posts.filter(post => post._id !== payload),
+        loading: false
+      }
     default:
       return state;
   }
